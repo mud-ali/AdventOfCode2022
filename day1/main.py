51a7fe818d859d01e51101bd62ae51dde40d73b3
mud-ali <mudali25@bergen.org>
2022-12-01 11:54:06 -0500

    init + day 1 part 1 sol

---
+++ day1/main.py
@@ -0,0 +1,14 @@
+def main():
+    with open('inputOne.txt', 'r') as file:
+        elves = file.read().split('\n\n')
+        mostCals = 0
+        for elf in elves:
+            calories = 0
+            for food in elf.split("\n"):
+                calories += int(food)
+            if calories > mostCals:
+                mostCals = calories
+        print(mostCals)
+
+if __name__ == '__main__':
+    main()
