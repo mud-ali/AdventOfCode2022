272da55bb6a571818a05b5ebb8e557a4bc38f819
mudasir <96320211+mud-ali@users.noreply.github.com>
2022-12-05 12:10:27 -0500

    day 5 part 1 sol

---
+++ day5/part1.py
@@ -0,0 +1,22 @@
+#im lazy so i'm going to hard code the original stacks in as lists
+def main():
+    s = []
+    with open('input.txt','r') as file:
+        for i in range(9):
+            s.append(list(file.readline().replace('\n','')))
+        instructions = file.readlines()
+        for cmd in instructions:
+            cmdF = list(map(int, cmd.replace("move ",'').replace('\n','').replace(" from ",',').replace(" to ",",").split(",")))
+            # print(s,cmdF[1]-1)
+            # print(cmdF[2]-1)
+            for i in range(cmdF[0]):
+                s[cmdF[2]-1].append(s[cmdF[1]-1].pop())
+    top = ''
+    for stack in s:
+        top += stack[-1]
+    
+    print(top)
+
+
+if __name__ == '__main__':
+    main()
